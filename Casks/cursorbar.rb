19bebe6ddd21b2bae148bc36c7aaf5399991f38a
Christian Johannesen <cj@serpier.com>
2026-09-17 09:58:53 +0200

    Update cursorbar cask to v1.6.5.

--- Casks/cursorbar.rb
+++ Casks/cursorbar.rb
@@ -1,8 +1,8 @@
 cask "cursorbar" do
-  version "1.6.4"
-  sha256 "60e75b8a9d5d8214ff5a32e8b4c095b6f677b7587f8173dd63629a12f7593ed3"
+  version "1.6.5"
+  sha256 "c3d423e3fa1e6d6407f631ded329159a0df71f15fc04a370d19ecb2712a9c991"
 
-  url "https://github.com/c-johannesen/cursorbar/releases/download/v1.6.4/CursorBar-1.6.4.zip"
+  url "https://github.com/c-johannesen/cursorbar/releases/download/v1.6.5/CursorBar-1.6.5.zip"
   name "CursorBar"
   desc "Menu bar app showing Cursor plan usage and remaining credits"
   homepage "https://github.com/c-johannesen/cursorbar"
